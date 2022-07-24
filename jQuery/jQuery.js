032678a65160a86067f3332b6c4f53a3f87f1a19
jltlanga <julitolanga@gmail.com>
2022-07-24 19:23:47 -0300

    jQuery review

---
+++ jQuery/jQuery.js
@@ -0,0 +1,29 @@
+//quando a página estiver carregada/disponível
+$(document).ready(function(){
+    //sintaxe jQuery utiliza seletores css
+    $(".btnHelloNome").click(function(){
+      let nome = $("#nome").val();
+     $("#texto").text(`${nome} !`);
+    });
+
+
+
+
+    $("#btnHello").click(function(){
+        $("h1 span").text("!!");
+    });
+
+    //ou
+    // $("button, input").mousemove(function(){
+    //     hello();
+    // });
+    /* -------------------------------------------------------------------------- */
+    /*                                  Uso de  de jQuery Mask                    */
+    /* -------------------------------------------------------------------------- */
+
+    $("#telefone").mask("(00) 00000 - 0000")
+}) 
+
+function hello() {
+    alert("Hello worldwide!");
+}
