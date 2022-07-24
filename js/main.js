6250b7afb95232eeb80c31a3bd5b35fff164225a
jltlanga <julitolanga@gmail.com>
2022-07-24 19:20:24 -0300

    atualiza js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
+$(document).ready(function() {
+  //Add all your jQuery elements hereby
+  $(".btn-buy").click(function() {
+    alert("Este produto não está desponível no momento");
+  });
+
+
+  // $(".menu-bars").click(function() {
+  //   $(".menu").toggleClass("mostrarEsconderMenu")
+  // });
+  
+
+});
+
 function abreFechaMenu() {
-    // navbar -tag nav
-    let navbar = document.querySelector(".menu");
-    navbar.classList.toggle("mostrarEsconderMenu");
-  }
+  // navbar -tag nav
+  let navbar = document.querySelector(".menu");
+  navbar.classList.toggle("mostrarEsconderMenu");
+}
+
